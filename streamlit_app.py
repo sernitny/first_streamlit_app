17a01404ee8768547ddd04bd44d7ab0daa295a1c
sernitny <157071925+sernitny@users.noreply.github.com>
2024-01-18 20:50:01 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,5 @@ streamlit.text('🥣 Omega 3 & blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Bolied Free-Range Egg')
 streamlit.text('🥑🍞 Avacado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
