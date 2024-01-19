77bed724c6eb3a2b38779b3765da12bd7b540498
sernitny <157071925+sernitny@users.noreply.github.com>
2024-01-18 20:49:31 -0500

    Update streamlit_app.py
    added symbols for each menu item and added avocado toast.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Bolied Free-Range Egg')
+streamlit.text('🥣 Omega 3 & blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Bolied Free-Range Egg')
+streamlit.text('🥑🍞 Avacado Toast')
