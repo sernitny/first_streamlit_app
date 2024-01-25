0d35ac5d9aa147bd79386f7dd6744360995c2ab5
sernitny <157071925+sernitny@users.noreply.github.com>
2024-01-24 21:28:43 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,4 +45,4 @@ streamlit.text(my_data_row)
 #second text box
 streamlit.header('Trying a second text box!')
 fruit_interest = streamlit.text_input('What fruit would you like to add now?','Banana?')
-streamlit.write('The user entered ', fruit_interest)
+streamlit.write('The user is intersted in ', fruit_interest)
