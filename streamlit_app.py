ff39a8d7bfe0550af01c0fac69aaf652411067ba
sernitny <157071925+sernitny@users.noreply.github.com>
2024-01-24 20:48:44 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,4 +29,4 @@ streamlit.text(fruityvice_response.json())
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
-
+import snowflake.connector
